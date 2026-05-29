ca29122d9c5b57acee1d51284eb1433e4bd15426
rrassya19-bit <rrassys19@gmail.com>
2026-05-29 16:04:48 +0700

    tambah pengecekan antrian kosong pada remove

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -48,5 +48,12 @@ public:
 
     void remove()
     {
+        // ccek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
     }
 };
